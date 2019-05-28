833ca6555054a0b8ede8c47006879f90fcb246f5
Adam Burkenpas <aurkenpas@gmail.com>
2019-05-27 20:26:34 -0400

    Add our stories content

--- src/pages/OurStories.vue
+++ src/pages/OurStories.vue
@@ -12,18 +12,18 @@
     <div class="container stories">
       <div class="row">
         <Story>
-          <g-image src="~/assets/images/stories/placeholder.png"/>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, minima libero nulla non accusamus fugit hic, neque rem nostrum Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur minus fuga sapiente, saepe necessitatibus suscipit! Itaque voluptas at, iusto ex corrupti ducimus deserunt architecto quia incidunt. Iure, iste eos fuga!</p>
+          <g-image src="~/assets/images/stories/patrick-story.png"/>
+          <p>Patrick is a large, robust man in his 50's, often described as "bigger than life." An 18-year old employee, he has risen through the ranks to an important position, in that he has to make critical decisions related to safety compliance in a field where public and employee safty is paramount.</p>
           <a href="#">Read More</a>
         </Story>
         <Story>
-          <g-image src="~/assets/images/stories/placeholder.png"/>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, minima libero nulla non accusamus fugit hic, neque rem nostrum Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur minus fuga sapiente, saepe necessitatibus suscipit! Itaque voluptas at, iusto ex corrupti ducimus deserunt architecto quia incidunt. Iure, iste eos fuga!</p>
+          <g-image src="~/assets/images/stories/maria-story.png"/>
+          <p>Maria was an administrative assistant who had worked for a large Fortune 500 company for 8 years. She has always worked quietly and mostly had kept to herself. Maria always received exceptional performace appraisals, but her manager had started to notice a decline in her performace over the prior 6 months...</p>
           <a href="#">Read More</a>
         </Story>
         <Story>
-          <g-image src="~/assets/images/stories/placeholder.png"/>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, minima libero nulla non accusamus fugit hic, neque rem nostrum Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur minus fuga sapiente, saepe necessitatibus suscipit! Itaque voluptas at, iusto ex corrupti ducimus deserunt architecto quia incidunt. Iure, iste eos fuga!</p>
+          <g-image src="~/assets/images/stories/neil-story.png"/>
+          <p>Neil had been a good and steady employee for over 25 years with his company, serving longer than any other existing employee. His loyalty and dedtication to the organization was unquestioned, and in fact, he always went beyond what would be anyone's expectations. Neil is an extremely reserved and sensitive man, and the first word...</p>
           <a href="#">Read More</a>
         </Story>
       </div>
