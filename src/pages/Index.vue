ad53b1f1684b0e37d2ead0d17f50201083a7d6cf
crh577 <crh577@gmail.com>
2020-11-19 10:26:26 -0500

    Update Index.vue

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,6 +1,7 @@
 <template>
   <Home>
     <div class="container container-home">
+      <!-- Testing GIT commit -->
       <div class="row">
         <div class="col-md-6 hero-left align-items-center">
           <g-image class="hero-image" src="~/assets/images/home/left-hero-desk.png" alt />
